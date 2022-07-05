8078e3340e84621543323320c25148fca082ecde
jcsax <105683373+jcsax@users.noreply.github.com>
2022-07-05 08:57:40 -0300

    Create apps and template from index.

---
+++ JC/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+
+#Inicio:
+
+def index(request):
+    return render(request, 'index.html')
---
+++ mdq_pets/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MdqPetsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mdq_pets'
